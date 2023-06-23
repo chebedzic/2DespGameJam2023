fe4b339949ad280e0f48cafa99f7dbf52287d1eb
Aleksa Cebedzic <>
2023-06-23 19:52:28 +0200

    Added some more stuff

--- Assets/Scripts/Game/Spawner.cs
+++ Assets/Scripts/Game/Spawner.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using DG.Tweening;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -21,33 +23,50 @@ namespace Game
                 return instance;
             }
         }
-        [SerializeField] private GameObject leftSamurai;
-        [SerializeField] private GameObject rightSamurai;
-        [SerializeField] private GameObject boss;
-        [SerializeField] private GameObject log;
-        [SerializeField] private float interval;
+        [SerializeField] private Transform leftSamurai;
+        [SerializeField] private Transform rightSamurai;
+        [SerializeField] private Transform boss;
+        [SerializeField] private Transform log;
+        [SerializeField] private AnimationCurve spawningOfEnemies;
+        [SerializeField] private float max = 100;
+
+        private Transform _player;
         public int spawned;
 
-        private IEnumerator StartGame()
+        void Awake()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        private IEnumerator Start()
         {
             while (true)
             {
-                int rnNUmber = Random.Range(0, 2);
+                int rnNUmber = Random.Range(0, 3);
+                Transform tr;
+                if (spawned == 99)
+                {
+                    yield return new WaitForSeconds(2);
+                    tr = Instantiate(boss, transform.position, quaternion.identity);
+                    tr.DOMove(_player.position, 2f, false).SetEase(Ease.Linear);
+                    yield break;;
+                }
                 switch (rnNUmber)
                 {
                     case 0:
-                        Instantiate(leftSamurai, transform.position, quaternion.identity);
+                        tr = Instantiate(leftSamurai, transform.position, quaternion.identity);
                         break;
                     case 1:
-                        Instantiate(leftSamurai, transform.position, quaternion.identity);
+                        tr =Instantiate(rightSamurai, transform.position, quaternion.identity);
                         break;
-                    case 2:
-                        //TODO: LOG!;
+                     default:
+                        tr =Instantiate(log, transform.position, quaternion.identity);
                         break;
                     
                 }
 
-                yield return new WaitForSeconds(interval);
+                tr.DOMove(_player.position, 2f, false).SetEase(Ease.Linear);
+                spawned++;
+                yield return new WaitForSeconds(spawningOfEnemies.Evaluate(spawned/max));
 
             }
         }
---
+++ Assets/Scripts/LoadGame/Loading.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] private GameObject enterName;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private TMP_InputField input;
+    [SerializeField] private Button btnOkName;
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnLeaderboards;
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("Name"))
+        {
+            ActivateMainMenu();
+        }
+        else
+        {
+            ActivateEnterName();
+        }
+
+        btnOkName.enabled = false;
+        btnPlay.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Game");
+        });
+        input.onValueChanged.AddListener((vl) =>
+        {
+            if (vl.Length > 5)
+            {
+                btnOkName.enabled = true;
+            }
+        });
+        btnOkName.onClick.AddListener(() =>
+        {
+            PlayerPrefs.SetString("Name", input.text);
+            ActivateMainMenu();
+        });
+    }
+    
+    private void ActivateMainMenu()
+    {
+        mainMenu.SetActive(true);
+        enterName.SetActive(false);
+    }
+
+    private void ActivateEnterName()
+    {
+        mainMenu.SetActive(false);
+        enterName.SetActive(true);
+    }
+}
